bc788ad1b55bbd84de9cf7e4740cf52ba66eb6f5
19AS19 <114105733+19AS19@users.noreply.github.com>
2022-12-08 01:24:35 +0000

    Add files via upload

---
+++ school/js assignments/exercises/index9.js
@@ -0,0 +1,18 @@
+//part.1
+/*let sum = 0;
+const number = prompt('num');
+let temp = number;
+while (temp>0) {
+    let rem = temp % 10;
+    sum += rem**number.length;
+    temp = Math.floor(temp/10);
+}
+if (sum==number) {
+    console.log(`${number} is armstrong`);
+}
+else {
+    console.log(`${number} isnt armstrong`);
+};
+*/
+//part.2
+var input = prompt("strong num")
